d1665fd2ac6b71a16dfdf451d4c2da6a3e28fbc4
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-31 17:43:11 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,7 +37,7 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully created song."
-    redirect to "songs/#{@song.slug}"
+    redirect "songs/#{@song.slug}"
 
   end
 
@@ -70,7 +70,7 @@ class SongsController < ApplicationController
 
     song.save
     flash[:message] = "Successfully updated song."
-    redirect to "songs/#{song.slug}"
+    redirect "songs/#{song.slug}"
   end
 
   get '/songs/:slug/edit' do
